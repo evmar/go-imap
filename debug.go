2c21c826ad87b36a91eabf8a63af6262c5069d83
Evan Martin <martine@danga.com>
2011-10-17 11:19:35 -0700

    check in debug.go

---
+++ debug.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io"
+	"os"
+	"log"
+)
+
+var logger *log.Logger
+
+type LoggingReader struct {
+	r io.Reader
+}
+
+func (r *LoggingReader) Read(p []byte) (int, os.Error) {
+	if logger == nil {
+		logger = log.New(os.Stderr, "", log.Ltime)
+	}
+
+	n, err := r.r.Read(p)
+	if err != nil {
+		return n, err
+	}
+	logger.Printf("<- %q", p[0:n])
+	return n, err
+}
