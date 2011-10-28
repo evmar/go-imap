96416a7c31a97b4aac9f51e51ab7b0bf0d77a3c4
Evan Martin <martine@danga.com>
2011-10-27 18:39:12 -0700

    wrap in object; pretty print

--- main.go
+++ main.go
@@ -20,14 +20,17 @@ func check(err os.Error) {
 	}
 }
 
-var statusChan chan string
-var netmon *netmonReader
+type UI struct {
+	statusChan chan string
+	netmon     *netmonReader
+}
 
-func status(format string, args ...interface{}) {
-	if statusChan != nil {
-		statusChan <- fmt.Sprintf(format, args...)
+func (ui *UI) status(format string, args ...interface{}) {
+	if ui.statusChan != nil {
+		ui.statusChan <- fmt.Sprintf(format, args...)
 	} else {
-		log.Printf(format, args...)
+		fmt.Printf(format, args...)
+		fmt.Printf("\n")
 	}
 }
 
@@ -62,9 +65,10 @@ func readExtra(im *imap.IMAP) {
 	}
 }
 
-func connect(useNetmon bool) *imap.IMAP {
+func (ui *UI) connect(useNetmon bool) *imap.IMAP {
 	user, pass := loadAuth("auth")
 
+	ui.status("connecting")
 	conn, err := tls.Dial("tcp", "imap.gmail.com:993", nil)
 	check(err)
 
@@ -73,30 +77,29 @@ func connect(useNetmon bool) *imap.IMAP {
 		r = newLoggingReader(r, 300)
 	}
 	if useNetmon {
-		netmon = newNetmonReader(r)
-		r = netmon
+		ui.netmon = newNetmonReader(r)
+		r = ui.netmon
 	}
 	im := imap.New(r, conn)
 	im.Unsolicited = make(chan interface{}, 100)
 
-	status("connecting")
 	hello, err := im.Start()
 	check(err)
-	status("server hello: %s", hello)
+	ui.status("server hello: %s", hello)
 
-	status("logging in")
+	ui.status("logging in")
 	resp, caps, err := im.Auth(user, pass)
 	check(err)
-	status("capabilities: %s", caps)
-	status("%s", resp)
+	ui.status("capabilities: %s", caps)
+	ui.status("%s", resp)
 
 	return im
 }
 
-func fetch(im *imap.IMAP, mailbox string) {
+func (ui *UI) fetch(im *imap.IMAP, mailbox string) {
 	examine, err := im.Examine(mailbox)
 	check(err)
-	status("%+v", examine)
+	ui.status("%+v", examine)
 	readExtra(im)
 
 	f, err := os.Create(mailbox + ".mbox")
@@ -104,7 +107,7 @@ func fetch(im *imap.IMAP, mailbox string) {
 	mbox := newMbox(f)
 
 	query := fmt.Sprintf("1:%d", examine.Exists)
-	status("fetching %s", query)
+	ui.status("fetching %s", query)
 
 	ch, err := im.FetchAsync(query, []string{"RFC822"})
 	check(err)
@@ -116,37 +119,53 @@ L:
 		switch r := r.(type) {
 		case *imap.ResponseFetch:
 			mbox.writeMessage(r.Rfc822)
-			status("got message %d/%d", i, examine.Exists)
+			ui.status("got message %d/%d", i, examine.Exists)
 			i++
 		case *imap.ResponseStatus:
-			status("complete %v\n", r)
+			ui.status("complete %v\n", r)
 			break L
 		}
 	}
 	readExtra(im)
 }
 
-func runFetch(im *imap.IMAP, mailbox string) {
-	statusChan := make(chan string)
+func (ui *UI) runFetch(im *imap.IMAP, mailbox string) {
+	ui.statusChan = make(chan string)
 	go func() {
-		fetch(im, mailbox)
-		close(statusChan)
+		defer func() {
+			if e := recover(); e != nil {
+				log.Printf("paniced %s", e)
+			}
+		}()
+		ui.fetch(im, mailbox)
+		close(ui.statusChan)
 	}()
 
 	ticker := time.NewTicker(1000 * 1000 * 1000)
 	status := ""
-	for statusChan != nil {
+	for ui.statusChan != nil {
+		overprint := true
 		select {
-		case s, closed := <-statusChan:
-			status = s
-			if closed {
-				statusChan = nil
+		case s, stillOpen := <-ui.statusChan:
+			if s != status {
+				overprint = false
+				status = s
 			}
-			ticker.Stop()
+			if !stillOpen {
+				ui.statusChan = nil
+				ticker.Stop()
+			}
 		case <-ticker.C:
-			netmon.Tick()
+			ui.netmon.Tick()
 		}
-		log.Printf("%.1fk/s %s\n", netmon.Bandwidth() / 1000.0, status)
+		if overprint {
+			fmt.Printf("\r\x1B[K")
+		} else {
+			fmt.Printf("\n")
+		}
+		if status != "" {
+			fmt.Printf("[%.1fk/s] %s", ui.netmon.Bandwidth() / 1000.0, status)
+		}
 	}
 }
 
@@ -169,9 +188,11 @@ func main() {
 	mode := args[0]
 	args = args[1:]
 
+	ui := new(UI)
+
 	switch mode {
 	case "list":
-		im := connect(false)
+		im := ui.connect(false)
 		mailboxes, err := im.List("", imap.WildcardAny)
 		check(err)
 		fmt.Printf("Available mailboxes:\n")
@@ -184,8 +205,8 @@ func main() {
 			fmt.Printf("must specify mailbox to fetch\n")
 			os.Exit(1)
 		}
-		im := connect(true)
-		runFetch(im, args[0])
+		im := ui.connect(true)
+		ui.runFetch(im, args[0])
 	default:
 		usage()
 	}
